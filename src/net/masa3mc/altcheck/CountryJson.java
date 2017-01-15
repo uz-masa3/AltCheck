3e15587f1a53b60f12eba7bb6c197c805afba16f
azure_crystal_ <stillcrystal@outlook.jp>
2017-01-16 06:25:02 +0900

    Add Javadoc Comment

--- src/net/masa3mc/altcheck/CountryJson.java
+++ src/net/masa3mc/altcheck/CountryJson.java
@@ -1,13 +1,32 @@
 package net.masa3mc.altcheck;
 
 public class CountryJson {
+
+  /** IPアドレス */
   public String ip;
+
+  /** 国コード */
   public String country_code;
+
+  /** 国名 */
   public String country_name;
+
+  /** 地域名 */
   public String region_name;
+
+  /** 街 */
   public String city;
+
+  /** 郵便番号 */
   public String zip_code;
+
+  /** タイムゾーン */
   public String time_zone;
-  public String latitude;		// ja: 軽度
-  public String longitude;	// ja: 緯度
+
+  /** 経度 */
+  public String latitude;
+
+  /** 緯度 */
+  public String longitude;
+
 }
