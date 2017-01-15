e64b69ea69a7d499f1f5065c7137401ea6b4af05
azure_crystal_ <stillcrystal@outlook.jp>
2017-01-16 07:24:17 +0900

    Javadocの追加

--- src/net/masa3mc/altcheck/UUIDJson.java
+++ src/net/masa3mc/altcheck/UUIDJson.java
@@ -1,6 +1,12 @@
 package net.masa3mc.altcheck;
 
+/**
+ * JSON structure object
+ */
 public class UUIDJson {
+  /** Player ID */
   public String id;
+
+  /** Player Name */
   public String name;
 }
