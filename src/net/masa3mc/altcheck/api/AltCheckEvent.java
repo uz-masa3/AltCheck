c7a859c8ea7e6ce3cdd175fc0eb86867ab511995
uz_masa3 <masa3masanori@gmail.com>
2017-01-18 03:06:15 +0900

    v1.5~

--- src/net/masa3mc/altcheck/api/AltCheckEvent.java
+++ src/net/masa3mc/altcheck/api/AltCheckEvent.java
@@ -1,32 +1,52 @@
 package net.masa3mc.altcheck.api;
 
+import java.util.List;
+
 import org.bukkit.command.CommandSender;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
 public class AltCheckEvent extends Event {
-  /*
-   *
-   * I'll add this api, when next update.
-   *
-   */
-  private static final HandlerList handlers = new HandlerList();
-  CommandSender checker;
+	/*
+	 *
+	 * added AltCheck v1.5
+	 *
+	 */
+	private static final HandlerList handlers = new HandlerList();
+	CommandSender checkedby;
+	String target;
+	boolean found;
+	List<String> alts;
 
-  public AltCheckEvent(CommandSender checker) {
-    this.checker = checker;
-  }
+	public AltCheckEvent(CommandSender CheckedBy, String Target, boolean found, List<String> alts) {
+		this.checkedby = CheckedBy;
+		this.target = Target;
+		this.found = found;
+		this.alts = alts;
+	}
 
-  public HandlerList getHandlers() {
-    return handlers;
-  }
+	public HandlerList getHandlers() {
+		return handlers;
+	}
 
-  public static HandlerList getHandlerList() {
-    return handlers;
-  }
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
 
-  public CommandSender getChecker() {
-    return checker;
-  }
+	public CommandSender getCheckedBy() {
+		return checkedby;
+	}
+
+	public String getTarget() {
+		return target;
+	}
+
+	public boolean isDataFound() {
+		return found;
+	}
+
+	public List<String> getAlts() {
+		return alts;
+	}
 
 }
