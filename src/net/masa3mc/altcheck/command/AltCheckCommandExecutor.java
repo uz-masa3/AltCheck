2cc16a0b4e8f15a26b1c68219202e3d0ef483ea9
uz_masa3 <masa3masanori@gmail.com>
2017-02-17 20:55:39 +0900

    Big fix

--- src/net/masa3mc/altcheck/command/AltCheckCommandExecutor.java
+++ src/net/masa3mc/altcheck/command/AltCheckCommandExecutor.java
@@ -2,7 +2,6 @@ package net.masa3mc.altcheck.command;
 
 import static org.bukkit.ChatColor.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -116,76 +115,6 @@ public final class AltCheckCommandExecutor implements CommandExecutor {
 					sender.sendMessage(AltCheck.ALTCHECK_PREFIX
 							+ Messages.noPermission);
 				}
-			} else if (args[0].equalsIgnoreCase("kickmessage")) {
-				if (sender.hasPermission("AltCheck.admin")) {
-					if (args.length > 1) {
-						StringBuilder message = new StringBuilder();
-						for (int i = 1; i < args.length; i++) {
-							message.append(args[i]).append(" ");
-						}
-						instance.getConfig().set("kickMessage", message.toString().trim());
-						instance.saveConfig();
-						instance.reloadConfig();
-						sender.sendMessage(AltCheck.ALTCHECK_PREFIX
-								+ Messages.save);
-					} else {
-						sender.sendMessage(translateAlternateColorCodes('&', "&7/altcheck kickmessage [Message]"));
-					}
-				} else {
-					sender.sendMessage(AltCheck.ALTCHECK_PREFIX
-							+ Messages.noPermission);
-				}
-			} else if (args[0].equalsIgnoreCase("maxalt")) {
-				if (sender.hasPermission("AltCheck.admin")) {
-					if (args.length == 2) {
-						try {
-							int max = Integer.parseInt(args[1]);
-							if (max > 0 && max < 1000) {
-								instance.getConfig().set("maxAlt", max);
-								instance.saveConfig();
-								instance.reloadConfig();
-								sender.sendMessage(AltCheck.ALTCHECK_PREFIX
-										+ Messages.save);
-							} else {
-								sender.sendMessage(translateAlternateColorCodes('&', "&7/altcheck maxalt [1 - 999]"));
-							}
-						} catch (NumberFormatException e) {
-							sender.sendMessage(AltCheck.ALTCHECK_PREFIX
-									+ Messages.enternumber);
-						}
-					} else {
-						sender.sendMessage(translateAlternateColorCodes('&', "&7/altcheck maxalt [1 - 999]"));
-					}
-				} else {
-					sender.sendMessage(AltCheck.ALTCHECK_PREFIX
-							+ Messages.noPermission);
-				}
-			} else if (args[0].equalsIgnoreCase("ignore")) {
-				if (sender.hasPermission("AltCheck.admin")) {
-					if (args.length == 2) {
-						if (args[1].length() == 36) {
-							if (!instance.getConfig().getStringList("IgnoreUUID").contains(args[1])) {
-								ArrayList<String> arraylist = new ArrayList<String>();
-								arraylist.add(args[1]);
-								for (String list : instance.getConfig().getStringList("IgnoreUUID")) {
-									arraylist.add(list);
-								}
-								instance.getConfig().set("IgnoreUUID", arraylist);
-								instance.saveConfig();
-								instance.reloadConfig();
-							}
-							sender.sendMessage(AltCheck.ALTCHECK_PREFIX
-									+ Messages.save);
-						} else {
-							sender.sendMessage(translateAlternateColorCodes('&', "&7/altcheck ignore [UUID]"));
-						}
-					} else {
-						sender.sendMessage(translateAlternateColorCodes('&', "&7/altcheck ignore [UUID]"));
-					}
-				} else {
-					sender.sendMessage(AltCheck.ALTCHECK_PREFIX
-							+ Messages.noPermission);
-				}
 			} else {
 				help(sender);
 			}
@@ -200,9 +129,6 @@ public final class AltCheckCommandExecutor implements CommandExecutor {
 				"&e AltCheck v" + Util.getVersion() + " by uz_masa3", // /* 2 */
 				"&7 - /altcheck check [IP / Player(Online)]", // /* 3 */
 				"&7 - /altcheck reload", // /* 4 */
-				"&7 - /altcheck kickmessage [Message]", // /* 5 */
-				"&7 - /altcheck maxalt [max]", // /* 6 */
-				"&7 - /altcheck ignore [UUID]" // /* 7 */
 		};
 		for (int i = 0; i < messages.length; i++) {
 			sender.sendMessage(translateAlternateColorCodes('&', messages[i]));
